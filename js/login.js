3e2c4ec3082c9bc426a4657a71eb5bc387333988
nadiaislam00 <islamnadia289@gmail.com>
2024-09-22 13:25:04 +0600

    home page added

--- js/login.js
+++ js/login.js
@@ -3,6 +3,15 @@ document.getElementById('login-btn').addEventListener('click', function(event){
 event.preventDefault();
 console.log('Login button clicked');
 
-const phoneNumber = document.getElementById('phone-number');
-console.log(phoneNumber.value);
+const phoneNumber = document.getElementById('phone-number').value;
+const pinNumber = document.getElementById('pin-number').value;
+console.log(phoneNumber, pinNumber);
+
+if(phoneNumber==='5' && pinNumber==='134'){
+    console.log('You are logged In');
+    window.location.href= './home.html';
+}
+else{
+   alert('Wrong PIN or Phone Number');
+}
 })
